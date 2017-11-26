e105b7855bc43b7c54085d4b9e76a0257b9d4f7a
StanisLoveSid <joingamings@gmail.com>
2017-11-26 18:39:58 +0200

    edited array

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@ include Math
 
 class IncorrectnessSolving
 
+  attr_accessor :n, :h, :eps, :alpha
+
   # first function value
   A_BIG = 1.00
   # last function value
@@ -10,17 +12,26 @@ class IncorrectnessSolving
   # boundaries of the integral
   A_SMALL = 0.00
   B_SMALL = 1.00
-  @@array_history = []
+  F_V = [1.1216266444377032,
+         1.326749744267982,
+         1.6759427493101955,
+         1.5567544929238246,
+         1.4017043386292973,
+         1.6133440801835377,
+         1.2633294507084925,
+         1.7546134946562288,
+         1.5641484944927924]
 
-  def initialize(n=10.0, h=0.14, eps=0.008, alpha=rand(0.0009..0.9))
+  def initialize(n=10.0, h=0.14, eps=0.008, alpha=0)
     @n = n
     @h_small = h
     @h_big = (B_SMALL - A_SMALL) / n
     @eps = eps
-    @alpha = alpha
+    @alpha = alpha.to_f
     @collection_of_x_i = x_i_array_creation
     @function_values = []
     @simpson_optimized_for_output = []
+    @array_history = []
   end
 
   def x_i_array_creation
@@ -43,29 +54,36 @@ class IncorrectnessSolving
   end
 
   def creation_of_minimized_array
-    function_values_generation
+  	function_values_generation
+    @array_history << @function_values
     until @h_small < @eps
       @h_small = @h_small/2
-      i = 0
-      until next_array_equal_to_previous?(@@array_history[i], @@array_history[i-1], i)
+      k = 0
+      minimized_array = []
+      until next_array_equal_to_previous?(@array_history[k], @array_history[k-1], k)
         (1..(@n - 1)).to_a.each do |i|
+          neutral = ((simpson_without_omega @array_history[k]) + (@alpha*(simpson_with_omega @array_history[k])))	
+          plus_h = ((simpson_without_omega functional_values_plus_h(k, i)) + (@alpha*(simpson_with_omega functional_values_plus_h(k, i))))
+          minus_h = ((simpson_without_omega functional_values_minus_h(k, i)) + (@alpha*(simpson_with_omega functional_values_minus_h(k, i))))
           search_for_min = []
-          search_for_min << ((simpson_without_omega @function_values) + (@alpha*(simpson_with_omega @function_values)))
-          search_for_min << ((simpson_without_omega functional_values_plus_h(i)) + (@alpha*(simpson_with_omega functional_values_plus_h(i))))
-          search_for_min << ((simpson_without_omega functional_values_minus_h(i)) + (@alpha*(simpson_with_omega functional_values_minus_h(i))))
+          search_for_min << neutral
+          search_for_min << plus_h
+          search_for_min << minus_h
           found = search_for_min.min
 
-          @@array_history << case found
-          when ((simpson_without_omega @function_values) + (@alpha*(simpson_with_omega @function_values))) then @simpson_optimized_for_output = @function_values
-          when ((simpson_without_omega functional_values_plus_h(i)) + (@alpha*(simpson_with_omega functional_values_plus_h(i)))) then @simpson_optimized_for_output = functional_values_minus_h(i)
-          when ((simpson_without_omega functional_values_minus_h(i)) + (@alpha*(simpson_with_omega functional_values_minus_h(i)))) then @simpson_optimized_for_output = functional_values_plus_h(i)
+          minimized_array << case found
+          when neutral then @array_history[k][i]
+          when plus_h then @array_history[k][i] + @h_small
+          when minus_h then @array_history[k][i] - @h_small
           end
         end
-        i += 1
+        minimized_array.unshift A_BIG
+        minimized_array.push B_BIG
+        @array_history << minimized_array
+        k += 1
       end
     end
-    
-    making_graph
+    @array_history.last
   end
 
   def next_array_equal_to_previous?(new_array, old_array, i)
@@ -81,7 +99,6 @@ class IncorrectnessSolving
     sum1 = A_BIG + B_BIG
     sum2 = 0
     @f_v = function_values
-
 
     (1..(@n.to_i - 1)).to_a.each do |i|
       sum2 += ((i % 2 == 1) ? 4 * @f_v[i] : 2 * @f_v[i])
@@ -121,26 +138,32 @@ class IncorrectnessSolving
     v + dv
   end
 
-  private
-
-  def functional_values_plus_h(i)
-  	rememerable = @function_values[i] += @h_small
-    @function_values.delete_at i
-    @function_values.insert(i, (rememerable))
+  def excellent
+    arr = []
+    12.times {|i| arr << ((i**3) + 1)}
+    arr
   end
 
-  def functional_values_minus_h(i)
-  	rememerable = @function_values[i] -= @h_small
-    @function_values.delete_at i
-    @function_values.insert(i, (rememerable))
+  private
+
+  def functional_values_plus_h(k, i)
+    rememerable = @array_history[k][i] + @h_small
+    @array_history[k].delete_at i
+    @array_history[k].insert(i, (rememerable))
+  end
+
+  def functional_values_minus_h(k, i)
+    rememerable = @array_history[k][i] - @h_small
+    @array_history[k].delete_at i
+    @array_history[k].insert(i, (rememerable))
   end
 
 end
 
-g = Gruff::Line.new
-g.title = 'Wow!  Look at this!'
-
-
-g.labels = IncorrectnessSolving.new.x_i_array_creation
-g.data :Simpson, IncorrectnessSolving.new.creation_of_minimized_array
-g.write("exciting#{rand(1.0..2000000.0)}.png")
+1.times do |alpha|
+  g = Gruff::Line.new
+  g.title = 'Wow!  Look at this!'
+  g.labels = IncorrectnessSolving.new.x_i_array_creation
+  g.data :Excellent, IncorrectnessSolving.new(10.0, 0.14, 0.008, 0.0042).creation_of_minimized_array
+  g.write("an_ending.png")
+end
